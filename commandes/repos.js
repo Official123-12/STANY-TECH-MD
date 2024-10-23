6cad47878a6bfea3552e56bc2d15569456ffa877
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-10-23 14:42:57 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "🎉", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/boniphace478/BONIPHACE-MD';
-  const img = 'https://files.catbox.moe/3vzjsl.jpeg';
+  const img = 'https://files.catbox.moe/b8dokx.jpeg';
 
   try {
     const response = await fetch(githubRepo);
