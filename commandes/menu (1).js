bd81cfb20f3d50cb8f2b452d64f67bebeb66b575
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-11-27 20:32:16 +0300

    Update menu (1).js

--- commandes/menu (1).js
+++ commandes/menu (1).js
@@ -1,33 +1,70 @@
 const util = require('util');
+
 const fs = require('fs-extra');
-const { famous } = require(__dirname + "/../framework/zokou"");
+
+const { zokou } = require(__dirname + "/../framework/zokou");
+
 const { format } = require(__dirname + "/../framework/mesfonctions");
+
 const os = require("os");
+
 const moment = require("moment-timezone");
+
 const s = require(__dirname + "/../set");
 
-famous({ nomCom: "menu5", categorie: "Général" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
-    let { cm } = require(__dirname + "/../framework/famous");
+
+
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+
+    let { cm } = require(__dirname + "/../framework//zokou");
+
     var coms = {};
+
     var mode = "public";
+
     
+
     if ((s.MODE).toLocaleLowerCase() != "yes") {
+
         mode = "private";
+
     }
 
+
+
+
+
+    
+
+
+
     cm.map(async (com, index) => {
+
         if (!coms[com.categorie])
+
             coms[com.categorie] = [];
+
         coms[com.categorie].push(com.nomCom);
+
     });
 
-    moment.tz.setDefault('Etc/GMT');
 
-    const temps = moment().format('HH:mm:ss');
-    const date = moment().format('DD/MM/YYYY');
 
-    let infoMsg = `
+    moment.tz.setDefault(s.TZ);
+
+
+
+// Créer une date et une heure en GMT
+
+const temps = moment().format('HH:mm:ss');
+
+const date = moment().format('DD/MM/YYYY');
+
+
+
+  let infoMsg =  `
 ╭────✧${s.BOT}✧────◆
 𝗙𝗔𝗠𝗢𝗨𝗦-𝗠𝗗 𝑽1
      ╭──────────────
